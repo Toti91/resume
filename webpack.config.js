9dd4ac5d9bacc4011c7f37f1e1abda12a860f259
Toti91 <torarinng@gmail.com>
2018-03-01 18:38:42 +0000

    Fixed mobile view

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,6 @@ const config = {
         filename: 'bundle.js',
         publicPath: '/'
     },
-    devtool: 'source-map',
-    watch: true,
     devServer: {
         port: 3000,
         contentBase: './src',
@@ -58,20 +56,17 @@ const config = {
                     outputPath: 'public/images/',
                     publicPath: '/public/images/',
                 }
-            }
-        ],
-        loaders: [
-            // the url-loader uses DataUrls. 
-            // the file-loader emits files. 
-            { 
-              test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-              loader: "url-loader?limit=10000&mimetype=application/font-woff" 
             },
-            { 
-              test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-              loader: "file-loader" 
-            },
-          ]
+            {
+                test: /\.(pdf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'public/files/',
+                    publicPath: '/public/files/',
+                }
+            }            
+        ]
     },
     plugins: [
         extractPlugin,
